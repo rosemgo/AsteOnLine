213080882ce4c033803a13b959bcb8167b3e403e
Rosario-VAIO\Rosario <rosariogoglia@gmail.com>
2018-01-20 15:00:29 +0100

    #6 Creazione interfaccia Banned Cookies, per gestire gli utenti bannati

---
+++ src/it/unisannio/sweng/rosariogoglia/dao/BannedCookiesDao.java
@@ -0,0 +1,29 @@
+package it.unisannio.sweng.rosariogoglia.dao;
+
+import it.unisannio.sweng.rosariogoglia.model.BannedCookies;
+
+/**
+ * Un utente registrato se bannato non può usufruire di tutti i servizi del sito, e non può effettuare acquisti.
+ * @author Rosario
+ *
+ */
+public interface BannedCookiesDao {
+	
+	public Integer insertBannedCookies(BannedCookies cookie);
+
+	/**
+	 * Verifica se l'utente con l'id passato come paramentro, è bannato o meno.
+	 * 
+	 * @param idUtente
+	 * @return true se l'utente è bannato, false in caso contrario
+	 */
+	public boolean checkUtenteRegistratoBanned(Integer idUtente);
+	
+	/**
+	 * Rimuovere un utente bannata dalla lista degli utenti bannati
+	 * 
+	 * @param idUtente
+	 */
+	public Integer deleteBannedCookies(Integer idUtente);
+		
+}

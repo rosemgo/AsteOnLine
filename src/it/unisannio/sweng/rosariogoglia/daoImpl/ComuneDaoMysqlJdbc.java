2b66c48d27af578499040d97fd9941dfed2d494d
Rosario-VAIO\Rosario <rosariogoglia@gmail.com>
2018-01-20 14:03:53 +0100

    Implementazione degli oggetti Comune Dao e Provincia Dao

---
+++ src/it/unisannio/sweng/rosariogoglia/daoImpl/ComuneDaoMysqlJdbc.java
@@ -0,0 +1,190 @@
+package it.unisannio.sweng.rosariogoglia.daoImpl;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+import it.unisannio.sweng.rosariogoglia.dao.ComuneDao;
+import it.unisannio.sweng.rosariogoglia.dbUtil.DatabaseUtil;
+import it.unisannio.sweng.rosariogoglia.model.Comune;
+import it.unisannio.sweng.rosariogoglia.model.Provincia;
+import it.unisannio.sweng.rosariogoglia.modelImpl.ComuneImpl;
+import it.unisannio.sweng.rosariogoglia.modelImpl.ProvinciaImpl;
+
+public class ComuneDaoMysqlJdbc implements ComuneDao{
+
+	Logger logger = Logger.getLogger(CategoriaDaoMysqlJdbc.class);
+	
+	public ComuneDaoMysqlJdbc (){
+		DOMConfigurator.configure("C:/Users/Rosario/git/AsteOnLine2/WebContent/WEB-INF/log4jConfig.xml");
+	}
+
+	public Comune getComuneById(int idComune) throws ClassNotFoundException, SQLException, IOException {
+		Comune comune = null;
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			connection = DatabaseUtil.getConnection();
+			
+			String sql = "SELECT * FROM comune, provincia " +
+					"WHERE comune.provincia_idprovincia = provincia.idprovincia " +
+					"AND" +
+					"comune.idcomune = ? ";
+			
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, idComune);
+			logger.debug("Select query " + pstmt.toString());
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				
+				comune = new ComuneImpl();
+				Provincia provincia = new ProvinciaImpl();
+				
+				provincia.setIdProvincia(rs.getInt("provincia.idprovincia"));
+				provincia.setNomeProvincia(rs.getString("provincia.nome_provincia"));
+				
+				comune.setIdComune(rs.getInt("comune.idcomune"));
+				comune.setNomeComune(rs.getString("comune.nome_comune"));
+				comune.setIdProvincia(rs.getInt("comune.provincia_idprovincia"));
+			
+				comune.setProvincia(provincia);
+			
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally{
+			rs.close();
+			pstmt.close();
+			connection.close();
+		}
+	
+		return comune;
+	}
+	
+	public List<Comune> getComuni() throws ClassNotFoundException, IOException{
+		logger.debug("in getComuni");
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<Comune> listaComuni = new ArrayList<Comune>();
+
+		try {
+				connection = DatabaseUtil.getConnection();
+			
+				Comune comune = null;
+				
+				String sql = "SELECT * FROM comune, provincia " +
+						"WHERE comune.provincia_idprovincia = provincia.idprovincia " +
+						"ORDER BY comune.nome_comune";
+				
+				pstmt = connection.prepareStatement(sql);
+				logger.debug("Select query " + pstmt.toString());
+				rs = pstmt.executeQuery();
+				while(rs.next()){
+					
+					comune = new ComuneImpl();
+					Provincia provincia = new ProvinciaImpl();
+					
+					provincia.setIdProvincia(rs.getInt("provincia.idprovincia"));
+					provincia.setNomeProvincia(rs.getString("provincia.nome_provincia"));
+					
+					comune.setIdComune(rs.getInt("comune.idcomune"));
+					comune.setNomeComune(rs.getString("comune.nome_comune"));
+					comune.setIdProvincia(rs.getInt("comune.provincia_idprovincia"));
+				
+					comune.setProvincia(provincia);
+					
+					listaComuni.add(comune);		
+					
+				}
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		finally{
+			try {
+				rs.close();
+				pstmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return listaComuni;
+		
+	}
+	
+	
+	public List<Comune> getComuniByIdProvincia(Integer idProvincia) throws ClassNotFoundException, IOException{
+		logger.debug("In getComuniByIdProvincia");
+		List<Comune> listaComuni = new ArrayList<Comune>();
+		Comune comune = null;
+		
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			connection = DatabaseUtil.getConnection();
+						
+			String sql = "SELECT * FROM comune, provincia " +
+					"WHERE comune.provincia_idprovincia = provincia.idprovincia " +
+					"AND comune.provincia_idprovincia = ? " +
+					"ORDER BY comune.nome_comune ";
+			
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, idProvincia);
+			logger.debug("Select query " + pstmt.toString());
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				
+				comune = new ComuneImpl();
+				Provincia provincia = new ProvinciaImpl();
+				
+				provincia.setIdProvincia(rs.getInt("provincia.idprovincia"));
+				provincia.setNomeProvincia(rs.getString("provincia.nome_provincia"));
+				
+				comune.setIdComune(rs.getInt("comune.idcomune"));
+				comune.setNomeComune(rs.getString("comune.nome_comune"));
+				comune.setIdProvincia(rs.getInt("comune.provincia_idprovincia"));
+			
+				comune.setProvincia(provincia);
+				
+				listaComuni.add(comune);		
+				
+				
+			}
+					
+				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				rs.close();
+				pstmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return listaComuni;
+		
+	}
+	
+	
+
+}
---
+++ src/it/unisannio/sweng/rosariogoglia/daoImpl/ProvinciaDaoMysqlJdbc.java
@@ -0,0 +1,131 @@
+package it.unisannio.sweng.rosariogoglia.daoImpl;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+import it.unisannio.sweng.rosariogoglia.dao.ComuneDao;
+import it.unisannio.sweng.rosariogoglia.dao.ProvinciaDao;
+import it.unisannio.sweng.rosariogoglia.dbUtil.DatabaseUtil;
+import it.unisannio.sweng.rosariogoglia.model.Comune;
+import it.unisannio.sweng.rosariogoglia.model.Provincia;
+import it.unisannio.sweng.rosariogoglia.modelImpl.ProvinciaImpl;
+
+public class ProvinciaDaoMysqlJdbc implements ProvinciaDao{
+
+	Logger logger = Logger.getLogger(ProvinciaDaoMysqlJdbc.class);
+	
+	public ProvinciaDaoMysqlJdbc (){
+		DOMConfigurator.configure("C:/Users/Rosario/git/AsteOnLine/WebContent/WEB-INF/log4jConfig.xml");
+	}
+
+	public List<Provincia> getProvince() throws ClassNotFoundException, IOException {
+		logger.debug("in getProvincie");
+		
+		List<Provincia> listaProvince = new ArrayList<Provincia>();
+		
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			connection = DatabaseUtil.getConnection();
+			
+			Provincia provincia = null;
+			String sql = "SELECT * FROM provincia " +
+					     "ORDER BY nome_provincia ";
+				
+			pstmt = connection.prepareStatement(sql);
+			logger.debug("Select query: " + pstmt.toString());
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				
+				provincia = new ProvinciaImpl();
+							
+				ComuneDao dao = new ComuneDaoMysqlJdbc();
+				List<Comune> listaComuni = dao.getComuniByIdProvincia(rs.getInt("idprovincia"));
+							
+				provincia.setIdProvincia(rs.getInt("idprovincia"));
+				provincia.setNomeProvincia(rs.getString("nome_provincia"));
+				provincia.setListaComuni(listaComuni);
+				
+				listaProvince.add(provincia);
+				
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+			
+		finally {
+			if (connection!=null) {
+				try {	
+					rs.close();
+					pstmt.close();
+					connection.close();
+				} catch (SQLException  e) {
+						
+						e.printStackTrace();
+				}
+					logger.debug("Connection chiusa");
+				}
+			}	
+		
+		return listaProvince;
+	}
+	
+	
+	public Provincia getProvinciaById(Integer idProvincia) throws ClassNotFoundException, SQLException, IOException {
+		logger.debug("in getProvinciaById");
+		Provincia provincia = null;
+		Connection connection = null;
+		PreparedStatement pstmt =null;
+		ResultSet rs = null;
+		
+		try {
+			connection = DatabaseUtil.getConnection();
+		
+			String sql = "SELECT * FROM provincia " +
+					"WHERE idprovincia = ? ";
+				
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, idProvincia);	
+			logger.debug("Select query: " + pstmt.toString());
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				provincia = new ProvinciaImpl();
+							
+				ComuneDao dao = new ComuneDaoMysqlJdbc();
+				List<Comune> listaComuni = dao.getComuniByIdProvincia(idProvincia);
+							
+				provincia.setIdProvincia(idProvincia);
+				provincia.setNomeProvincia(rs.getString("nome_provincia"));
+				provincia.setListaComuni(listaComuni);
+							
+			}
+		}
+			
+			finally {
+				if (connection!=null) {
+					try {
+						rs.close();
+						pstmt.close();
+						connection.close();
+					} catch (SQLException  e) {
+						
+						e.printStackTrace();
+					}
+					logger.debug("Connection chiusa");
+				}
+			}	
+		return provincia;
+	}
+
+	
+}

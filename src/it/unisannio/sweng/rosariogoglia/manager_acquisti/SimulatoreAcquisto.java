fad9bafd5898622d64a0094484496b971dce20ca
Rosario-VAIO\Rosario <rosariogoglia@gmail.com>
2018-01-23 13:02:19 +0100

    #6 Verifica la validità del codice della carta di credito inserita secondo l'algoritmo Luhn

---
+++ src/it/unisannio/sweng/rosariogoglia/manager_acquisti/SimulatoreAcquisto.java
@@ -0,0 +1,16 @@
+package it.unisannio.sweng.rosariogoglia.manager_acquisti;
+
+public class SimulatoreAcquisto {
+
+	
+	/*Il metodo simula il controllo pagamento. Restituisce un numero casuale compreso da 0 a 10. In caso di numero minore di 8 il pagamento andrà a buon fine, in caso di numero maggiore di 10 il pagamento non sarà convalidato*/
+    public boolean pagamento () {
+        boolean result = false;
+        int numCasuale = (int) Math.random() * 10;
+        if (numCasuale < 8){
+            result = true;
+        }
+        return result;
+    }
+   
+}

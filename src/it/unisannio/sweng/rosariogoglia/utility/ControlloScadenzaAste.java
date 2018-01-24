dd4fb6ddd5b01752c488b90d4594b1526d576156
Rosario-VAIO\Rosario <rosariogoglia@gmail.com>
2018-01-24 12:22:34 +0100

    Fixes #9

--- src/it/unisannio/sweng/rosariogoglia/utility/ControlloScadenzaAste.java
+++ src/it/unisannio/sweng/rosariogoglia/utility/ControlloScadenzaAste.java
@@ -4,10 +4,9 @@ import it.unisannio.sweng.rosariogoglia.dao.InserzioneDao;
 import it.unisannio.sweng.rosariogoglia.daoImpl.InserzioneDaoMysqlJdbc;
 import it.unisannio.sweng.rosariogoglia.model.Inserzione;
 
-import java.io.IOException;
+
 import java.util.Date;
 import java.util.Vector;
-
 
 
 public class ControlloScadenzaAste extends Thread{
@@ -43,7 +42,7 @@ public class ControlloScadenzaAste extends Thread{
 		
 			if(!this.listaInserzioni.contains(this.inserzione.getIdInserzione())){
 				this.listaInserzioni.add(this.inserzione.getIdInserzione());
-				System.out.println("la lista non conteneva l'inserzione, ora è stata aggiunta");
+				System.out.println("non contiene l'inserzione");
 			}
 			
 			int posizioneIdInserzione = this.listaInserzioni.indexOf(this.inserzione.getIdInserzione());
@@ -69,37 +68,63 @@ public class ControlloScadenzaAste extends Thread{
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-											
+			
+				//VERIFICO CHE DOPO IL PERIODO DI ATTESA L'INSERZIONE SIA SEMPRE PRESENTE NEL DB E NON SIA STATA MODIFICATA!!!
+				//PER QUESTA RAGIONE LA PRELEVO DAL DBUTILIZZANDO L'ID CHE PRECEDENTEMENTE AVEVO SALVATO
+				
+				
+				synchronized (this.listaInserzioni.get(posizioneIdInserzione)) {
+				
+					System.out.println("ARRIVA PRIMA L'INSERZIONE: " + this.inserzione.getIdInserzione());
+					
+					this.inserzione = dao.getInserzioneById(this.listaInserzioni.get(posizioneIdInserzione)); 
+					
+					System.out.println("prelevo di nuovo l'inserzione");
+					
+				}
+								
 			}
+			
+			//SE L'INSERZIONE ESISTE ANCORA(DOPO IL PERIODO DI ATTESA) EFFETTUO IL CONTROLLO SULLA SCADENZA(SE L'INSERZIONE HA SUBITO DELLE MODIFICHE SI ENTRARE IN QUESTO PRIMO IF, MA NN SARA' EFFETTUATO L'AGGIORNAMENTO STATO)
+			//NEL CASO IN CUI NON SI ABBIA UN PERIODO DI ATTESA PERCHè L'INSERZIONE E' GIà SCADUTA, ABBIAMO LA CERTEZZA CHE SIA DIVERSA DA NULL 
+			if(this.inserzione != null){
+				
+				System.out.println("avvio il thread");
+					
+				/*
+				 *  Per ogni prodotto controlla che sia in asta e che la data di fine asta sia minore di quella attuale
+				 *  In tal caso aggiorna lo stato del prodotto ad aggiudicato (se c'è un acquirente) o scaduto (se non c'è un acquirente) 
+				 */
+				Date fineAsta = this.inserzione.getDataScadenza();
+				Date odierna = new Date();
 				
 				String statoInserzione = this.inserzione.getStato();
 				
 				//solo un thread alla volta può effettuare operazione con lo stesso idinserzione (quindi sulla stessa RISORSA!!!)
 				synchronized (this.listaInserzioni.get(posizioneIdInserzione)) {
-							
+									
+					if(statoInserzione.equals("in asta") && (fineAsta.compareTo(odierna) <= 0)){
 							
 						/*Se non c'è nessun acquirente l'idacquirente sarà 0*/
-						if(this.inserzione.getIdAcquirente() == 0){
+						if(inserzione.getIdAcquirente() == 0){
 							statoInserzione = "scaduta";
 						}
 						else{
 							statoInserzione = "aggiudicata";
 						}
 							
-						//Esegue l'aggiornamento dell'inserzione nel database
+						// Esegue l'aggiornamento dell'inserzione nel database
 						dao.updateStatoInserzione(statoInserzione, this.inserzione.getIdInserzione());
 						
-						/*
-						 * INSERIRE L'INVIO DELLA MAIL PER GLI UTENTI
-						 */
-												
 						System.out.println("STATO ASTE AGGIORNATO dal thread con sincro");
 					}
 				
 				}
 					
+				
+			}
 			
 		}	
 	
-
+}
 	

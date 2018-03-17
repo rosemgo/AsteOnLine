e068e8b9a956ffeeb25ea22cc6517fe80cbce8df
marcobelf <marco.belfiore88@gmail.com>
2018-03-17 17:46:05 +0100

    #53 Aggiornamento javadoc

--- src/it/unisannio/sweng/rosariogoglia/dao/ComuneDao.java
+++ src/it/unisannio/sweng/rosariogoglia/dao/ComuneDao.java
@@ -9,9 +9,35 @@ import it.unisannio.sweng.rosariogoglia.model.Comune;
 
 public interface ComuneDao {
 	
+	/**
+	 * Questo metodo carica il comune avente l'id passato come parametro
+	 * 
+	 * @param idComune
+	 * 
+	 * @return Restituisce il comune
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @throws IOException
+	 */
+	
 	public Comune getComuneById(int idComune) throws ClassNotFoundException, SQLException, IOException;
 	
+	/**
+	 * Questo metodo visualizza la lista di tutti i comuni
+	 * 
+	 * @return La lista contenente tutti i comuni
+	 */
+	
 	public List<Comune> getComuni();
+	
+	/**
+	 * Questo metodo visualizza tutti i comuni aventi l'idProvincia passato come parametro.
+	 * 
+	 * @param idProvincia
+	 * 
+	 * @return La lista dei comuni appartenenti ad una provincia
+	 */
 	
 	public List<Comune> getComuniByIdProvincia(Integer idProvincia);
 	

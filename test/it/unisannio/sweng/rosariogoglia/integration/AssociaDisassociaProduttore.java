3fff9815e6725dd7ec39a624fafefdc717ed5ce9
marcobelf <marco.belfiore88@gmail.com>
2018-07-14 18:37:52 +0200

    #75 Test di inserimento/cancellazione di un produttore e associazione/disassociazione del produttore inserito ad una categoria.

---
+++ test/it/unisannio/sweng/rosariogoglia/integration/AssociaDisassociaProduttore.java
@@ -0,0 +1,111 @@
+package it.unisannio.sweng.rosariogoglia.integration;
+
+
+
+	import java.util.regex.Pattern;
+	import java.util.concurrent.TimeUnit;
+	import org.junit.*;
+	import static org.junit.Assert.*;
+	import static org.hamcrest.CoreMatchers.*;
+	import org.openqa.selenium.*;
+	import org.openqa.selenium.firefox.FirefoxDriver;
+	import org.openqa.selenium.support.ui.Select;
+
+	
+	
+	public class AssociaDisassociaProduttore {
+	  private WebDriver driver;
+	  private String baseUrl;
+	  private boolean acceptNextAlert = true;
+	  private StringBuffer verificationErrors = new StringBuffer();
+
+	  @Before
+	  public void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "C:/Users/Rosario/git/AsteOnLine/geckodriver-v0.21.0-win64/geckodriver.exe");
+	    driver = new FirefoxDriver();
+	    baseUrl = "http://localhost:30000/AsteOnLine/index";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
+
+	  @Test
+	  public void testAggiungiEAssociaProduttore() throws Exception {
+	    driver.get("http://localhost:30000/AsteOnLine/index");
+	    driver.findElement(By.name("nick")).click();
+	    driver.findElement(By.name("nick")).clear();
+	    driver.findElement(By.name("nick")).sendKeys("ros7");
+	    driver.findElement(By.name("password")).click();
+	    driver.findElement(By.name("password")).clear();
+	    driver.findElement(By.name("password")).sendKeys("rosario");
+	    driver.findElement(By.id("bottone-Accedi")).click();
+	    driver.findElement(By.xpath("//a[2]/strong")).click();
+	    driver.findElement(By.xpath("//a[3]/strong")).click();
+	    driver.findElement(By.id("campoTesto")).click();
+	    driver.findElement(By.id("campoTesto")).clear();
+	    driver.findElement(By.id("campoTesto")).sendKeys("Nokia");
+	    driver.findElement(By.name("website")).click();
+	    driver.findElement(By.name("website")).clear();
+	    driver.findElement(By.name("website")).sendKeys("www.nokia.com");
+	    driver.findElement(By.id("bottone")).click();
+	    driver.findElement(By.xpath("//a[2]/strong")).click();
+	    driver.findElement(By.id("cate")).click();
+	    new Select(driver.findElement(By.id("cate"))).selectByVisibleText("Cellulari");
+	    driver.findElement(By.xpath("(//option[@value='3'])[3]")).click();
+	    driver.findElement(By.id("prod")).click();
+	    new Select(driver.findElement(By.id("prod"))).selectByVisibleText("Nokia");
+	    driver.findElement(By.xpath("//option[@value='25']")).click();
+	    driver.findElement(By.xpath("(//input[@id='bottone'])[7]")).click();
+	    driver.findElement(By.id("categoria2")).click();
+	    new Select(driver.findElement(By.id("categoria2"))).selectByVisibleText("Cellulari");
+	    driver.findElement(By.xpath("(//option[@value='3'])[4]")).click();
+	    driver.findElement(By.id("produttore2")).click();
+	    new Select(driver.findElement(By.id("produttore2"))).selectByVisibleText("Nokia");
+	    driver.findElement(By.xpath("//option[@value='25']")).click();
+	    driver.findElement(By.xpath("(//input[@id='bottone'])[9]")).click();
+	    driver.findElement(By.xpath("//a[7]/strong")).click();
+	  }
+
+	  @After
+	  public void tearDown() throws Exception {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
+	  }
+
+	  private boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+	  }
+
+	  private boolean isAlertPresent() {
+	    try {
+	      driver.switchTo().alert();
+	      return true;
+	    } catch (NoAlertPresentException e) {
+	      return false;
+	    }
+	  }
+
+	  private String closeAlertAndGetItsText() {
+	    try {
+	      Alert alert = driver.switchTo().alert();
+	      String alertText = alert.getText();
+	      if (acceptNextAlert) {
+	        alert.accept();
+	      } else {
+	        alert.dismiss();
+	      }
+	      return alertText;
+	    } finally {
+	      acceptNextAlert = true;
+	    }
+	  }
+	}
+
+
+

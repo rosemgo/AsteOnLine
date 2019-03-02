2ec78debd5245c048eb0bd7707a6a664b8cc1af1
marcobelf <marco.belfiore88@gmail.com>
2019-03-02 14:14:42 +0100

    #75 Aggiunti test produttore

--- test/it/unisannio/sweng/rosariogoglia/daoImpl/ProduttoreDaoMysqlJdbcTest.java
+++ test/it/unisannio/sweng/rosariogoglia/daoImpl/ProduttoreDaoMysqlJdbcTest.java
@@ -3,7 +3,12 @@ package it.unisannio.sweng.rosariogoglia.daoImpl;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.sql.Array;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -14,7 +19,7 @@ import it.unisannio.sweng.rosariogoglia.modelImpl.ProduttoreImpl;
 public class ProduttoreDaoMysqlJdbcTest {
 
 	@Test
-	public void test() throws ClassNotFoundException, SQLException, IOException {
+	public void testInserimentoRimozioneProduttore() throws ClassNotFoundException, SQLException, IOException {
 
 		ProduttoreDao dao = new ProduttoreDaoMysqlJdbc();
 		Produttore writingProduttore = new ProduttoreImpl();
@@ -22,18 +27,49 @@ public class ProduttoreDaoMysqlJdbcTest {
 		Integer autoIncKey = -1;
 		writingProduttore.setNome("TestProduttore");
 		writingProduttore.setWebsite("Website Test");
-		autoIncKey = dao.insertProduttoreTest(writingProduttore);
+		autoIncKey = dao.insertProduttore(writingProduttore);
 		//writingProduttore.setIdProduttore(autoIncKey);
 
 		System.out.println("id produttore: " + writingProduttore.getIdProduttore());
 		
-		readingProduttore = dao.getProduttoreByNomeTest(writingProduttore.getNome());
+		readingProduttore = dao.getProduttoreByNome(writingProduttore.getNome());
 
 		assertEquals(readingProduttore.getNome(), writingProduttore.getNome());
 		assertEquals(readingProduttore.getIdProduttore(), writingProduttore.getIdProduttore());
-		
-		Integer deletedRows = dao.deleteProduttoreTest(readingProduttore.getIdProduttore());
+		boolean result = false;
+		result = dao.checkDeleteProduttore(writingProduttore.getIdProduttore());
+		assertEquals(true, result);
+		Integer deletedRows = dao.deleteProduttore(readingProduttore.getIdProduttore());
 		assertEquals(deletedRows, (Integer)1);
+		
+	}
+	
+	@Test
+	public void testGetProduttore() {
+		
+		//lista produttori nel db
+		
+		ProduttoreDao produttoreDao = new ProduttoreDaoMysqlJdbc();
+		List<Produttore> result = new ArrayList<Produttore>();
+		result = produttoreDao.getProduttori();
+		assertEquals("Samsung", result.get(0).getNome());
+		assertEquals("Canon", result.get(1).getNome());
+		
+		//metodo che restituisce un produttora dall'id
+			
+		Produttore produttore = new ProduttoreImpl();
+		produttore = produttoreDao.getProduttoreById(3);
+		assertEquals("HP", produttore.getNome());
+		
+		//lista produttori per categoria
+			
+		result.removeAll(result);
+		result = produttoreDao.getProduttoriByIdCategoria(3);
+		assertEquals("Samsung", result.get(0).getNome());
+		assertEquals("LG", result.get(1).getNome());
+		assertEquals("Apple", result.get(2).getNome());
+		assertEquals("Huawei", result.get(3).getNome());
+		
 		
 	}
 	

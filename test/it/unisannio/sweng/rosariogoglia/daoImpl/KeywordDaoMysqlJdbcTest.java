7a17d8409c73b6005a5f005d3a0d2ce90978c017
marcobelf <marco.belfiore88@gmail.com>
2019-03-02 14:10:33 +0100

    #75 Aggiunti test Keyword.

--- test/it/unisannio/sweng/rosariogoglia/daoImpl/KeywordDaoMysqlJdbcTest.java
+++ test/it/unisannio/sweng/rosariogoglia/daoImpl/KeywordDaoMysqlJdbcTest.java
@@ -16,28 +16,28 @@ import org.junit.Test;
 public class KeywordDaoMysqlJdbcTest {
 
 	@Test
-	public void testKeywordDaoMysqlJdbc() throws ClassNotFoundException, SQLException, IOException {
+	public void testInserimentoRimozioneKeyword() throws ClassNotFoundException, SQLException, IOException {
 		KeywordDao dao = new KeywordDaoMysqlJdbc();
 		Keyword writingKeyword = new KeywordImpl();
 		Keyword readingKeyword = null;
 		int autoIncKey = -1;
 		
 		writingKeyword.setKeyword("KeywordTest");
-		autoIncKey = dao.insertKeywordTest(writingKeyword);
+		autoIncKey = dao.insertKeyword(writingKeyword);
 		writingKeyword.setIdKeyword(autoIncKey);
 
-		readingKeyword = dao.getKeywordByWordTest(writingKeyword.getKeyword());
+		readingKeyword = dao.getKeywordByWord(writingKeyword.getKeyword());
 
 		assertEquals(readingKeyword.getKeyword(), writingKeyword.getKeyword());
 		assertEquals(readingKeyword.getIdKeyword(), writingKeyword.getIdKeyword());
 		
-		Integer deletedRows = dao.deleteKeywordTest(readingKeyword.getIdKeyword());
+		Integer deletedRows = dao.deleteKeyword(readingKeyword.getIdKeyword());
 		assertEquals(deletedRows, (Integer)1);
 	
 		
-		// verificare la lista di parole choave associate ad un prodotto
+		// verificare la lista di parole chiave associate ad un prodotto
 		int idProdotto = 3;
-		List<Keyword> lista = dao.getKeywordByIdProdottoTest(idProdotto);
+		List<Keyword> lista = dao.getKeywordByIdProdotto(idProdotto);
 		
 		for(int i=0; i<lista.size(); i++){
 			System.out.println("Parola " + lista.get(i).toString());
@@ -45,5 +45,17 @@ public class KeywordDaoMysqlJdbcTest {
 		
 		
 	}
+	
+	@Test
+	public void testKeywordbyId() {
+		
+				//get Keyword by id
+		
+				KeywordDao dao = new KeywordDaoMysqlJdbc(); 
+				Keyword result = new KeywordImpl();
+				result = dao.getKeywordById(22);
+				assertEquals(result.getKeyword() , "Pc");
+}
+	
 
 }

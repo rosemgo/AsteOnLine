378fa84a569cfdeca8be8117af9f08a6ba2c52d4
Rosario-VAIO\Rosario <rosariogoglia@gmail.com>
2019-03-02 18:34:16 +0100

    #75 Aggiornamento test

--- test/it/unisannio/sweng/rosariogoglia/integration/InserimentoRimozioneProdottoAdmin.java
+++ test/it/unisannio/sweng/rosariogoglia/integration/InserimentoRimozioneProdottoAdmin.java
@@ -1,16 +1,18 @@
 package it.unisannio.sweng.rosariogoglia.integration;
 
-
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+
 import org.junit.*;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class InserisciRimuoviProdotto {
+public class InserimentoRimozioneProdottoAdmin {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
@@ -18,17 +20,15 @@ public class InserisciRimuoviProdotto {
 
   @Before
   public void setUp() throws Exception {
-    System.setProperty("webdriver.gecko.driver", "C:/Users/Rosario/git/AsteOnLine/geckodriver-v0.21.0-win64/geckodriver.exe");
-    driver = new FirefoxDriver();
-    baseUrl = "http://localhost:30000/AsteOnLine/index";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  System.setProperty("webdriver.gecko.driver", "C:/Users/Rosario/git/AsteOnLine/geckodriver-v0.21.0-win64/geckodriver.exe");
+	  driver = new FirefoxDriver();
+	  baseUrl = "http://localhost:30000/AsteOnLine/index";
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
-  public void testInserisciProdotto() throws Exception {
-	  driver.get("http://localhost:30000/AsteOnLine/ServletLogout");
-	    driver.findElement(By.name("nick")).click();
-	    driver.findElement(By.xpath("//a[@id='logo']")).click();
+  public void testInserimentoRimozioneProdottoAdmin() throws Exception {
+	  driver.get("http://localhost:30000/AsteOnLine/index");
 	    driver.findElement(By.name("nick")).click();
 	    driver.findElement(By.name("nick")).clear();
 	    driver.findElement(By.name("nick")).sendKeys("ros7");
@@ -36,27 +36,29 @@ public class InserisciRimuoviProdotto {
 	    driver.findElement(By.name("password")).clear();
 	    driver.findElement(By.name("password")).sendKeys("rosario");
 	    driver.findElement(By.id("bottone-Accedi")).click();
-	    driver.findElement(By.xpath("//a[4]/strong")).click();
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Produttori |'])[1]/following::strong[1]")).click();
 	    driver.findElement(By.id("categoria")).click();
-	    new Select(driver.findElement(By.id("categoria"))).selectByVisibleText("Cellulari");
-	    driver.findElement(By.xpath("//option[@value='3']")).click();
+	    new Select(driver.findElement(By.id("categoria"))).selectByVisibleText("Elettronica");
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Prodotti'])[1]/following::option[6]")).click();
 	    driver.findElement(By.id("produttore")).click();
-	    new Select(driver.findElement(By.id("produttore"))).selectByVisibleText("Huawei");
-	    driver.findElement(By.xpath("(//option[@value='7'])[2]")).click();
+	    new Select(driver.findElement(By.id("produttore"))).selectByVisibleText("Sony");
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Prodotti'])[1]/following::option[12]")).click();
 	    driver.findElement(By.id("campoTesto")).click();
 	    driver.findElement(By.id("campoTesto")).clear();
 	    driver.findElement(By.id("campoTesto")).sendKeys("Test Prodotto");
 	    driver.findElement(By.id("bottone")).click();
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Produttori |'])[1]/following::strong[1]")).click();
 	    driver.findElement(By.id("categoria2")).click();
-	    new Select(driver.findElement(By.id("categoria2"))).selectByVisibleText("Cellulari");
-	    driver.findElement(By.xpath("(//option[@value='3'])[2]")).click();
+	    new Select(driver.findElement(By.id("categoria2"))).selectByVisibleText("Elettronica");
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Seleziona categoria e produttore'])[1]/following::option[6]")).click();
 	    driver.findElement(By.id("produttore2")).click();
-	    new Select(driver.findElement(By.id("produttore2"))).selectByVisibleText("Huawei");
-	    driver.findElement(By.xpath("(//option[@value='7'])[3]")).click();
+	    new Select(driver.findElement(By.id("produttore2"))).selectByVisibleText("Sony");
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Seleziona categoria e produttore'])[1]/following::option[12]")).click();
 	    driver.findElement(By.id("prodotto2")).click();
 	    new Select(driver.findElement(By.id("prodotto2"))).selectByVisibleText("Test Prodotto");
-	    //driver.findElement(By.xpath("//option[@value='56']")).click();
-	    driver.findElement(By.xpath("(//input[@id='bottone'])[3]")).click();
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Seleziona categoria e produttore'])[1]/following::option[16]")).click();
+	    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Seleziona categoria e produttore'])[1]/following::input[4]")).click();
+	    
   }
 
   @After
@@ -101,3 +103,4 @@ public class InserisciRimuoviProdotto {
     }
   }
 }
+
